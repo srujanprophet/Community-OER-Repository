040830dff2ad89f2bb2c4765a396c96dc725597c
Srujan <srujanprophet@gmail.com>
2018-05-31 18:22:32 +0530

    Add test api

--- CollabOER-project/CollabOER/views.py
+++ CollabOER-project/CollabOER/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import requests
+
 
 def homepage(request):
 	return render(request,'home.html')
@@ -7,6 +9,9 @@ def login(request):
 	username = request.GET['username']
 	password = request.GET['password']
 
+	response = requests.get('http://freegeoip.net/json/')
+	geodata = response.json()
+
 	print(username, password)
 	msg = "Successful login"
-	return render(request,'show.html',{'message':msg})
+	return render(request,'show.html',{'message':msg,'ip': geodata['ip'],'country':geodata['country_name']})
