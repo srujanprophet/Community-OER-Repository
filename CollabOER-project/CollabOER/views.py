deb63441d7e8e11e9930de6f8b462baf62234c8b
DURGESHBARWAL <durgeshbarwal@gmail.com>
2018-06-05 15:06:28 +0530

    last modified for cookies

--- CollabOER-project/CollabOER/views.py
+++ CollabOER-project/CollabOER/views.py
@@ -7,6 +7,8 @@ def homepage(request):
 	return render(request,'home.html')
 
 def login(request):
+
+	#login api calling	
 	url = 'http://127.0.0.1:80/rest/login'
 	head = {'email': 'durgeshbarwal@gmail.com', 'password': '1773298936'}	
 	r = requests.post(url, data=head)
@@ -14,23 +16,44 @@ def login(request):
 	print(r.status_code)
 	print(r.headers)
 	
-	#taking cookie
+
+
+	#extracting  cookies
 	s = r.headers['Set-Cookie']
 	end = s.find(';')
 	print(s[:end])
 
 	k = (r.cookies['JSESSIONID'])
 	print(k)
-	print(k)
+	
 	#cookie vairable
-	cook = {'JSESSIONID': k}
+	#cook = {'JSESSIONID': k}
 	
-	#url = 'http://127.0.0.1:80/rest/communities'
+	cookies = dict(JSESSIONID=k)
 	
-	#r2 = requests.post()	
+	#jar = requests.cookie.RequestsCookieJar()
+	#jar.set('JSESSIONID', k)	
+	
+	
 
+	#for posting an community through api
+	url2 = 'http://127.0.0.1:80/rest/communities'
+	#da2 = { 'name': 'Gymkhana', 'copyrightText': '', 'introductoryText': 'Welcome to the Sport Club', 'shortDescription': 'This', 'sidebarText': '' }
+	da2 = { "name": "Gymkhana", "copyrightText": "", "introductoryText": "Welcome to the Sport Club", "shortDescription": "This", "sidebarText": "" }
+	head2 = {'Content-Type': 'application/json'}
+	r2 = requests.post(url2, headers=head2, data=da2, cookies={'JESSIONID': k})	
 	
-
+	print(r2.text)
+	print(r2.headers)
+	print(r2.url)
+	print(r2.cookies)
+		
+	
+	print(r2.status_code)
 	msg = "Getting all the Communities in the System"
 	return render(request,'show.html',{'message':msg})
+
+
+
+
 
